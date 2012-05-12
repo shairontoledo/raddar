b8c1c1e58574d99353b0e5f690b6ac36e0dea10b
Volmer Soares <lrvolmer@gmail.com>
2012-05-11 21:17:54 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raddar::Application.config.secret_token = 'ffcab4ccf26bcddd184dd7b6104344cb7f37593d94e9837075417d97df05c4c8fde2ccac4780d5ddd215e09c4726a035890652edcb98b4398918c8123a2d48ed'
