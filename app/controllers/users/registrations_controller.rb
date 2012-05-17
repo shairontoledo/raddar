310ad6a6053a12b99957d297f2f7d6c7c781f6ca
Volmer Soares <volmer.soares@ebc.com.br>
2012-05-17 18:21:53 -0300

    Update e-mail with confirmation feature

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,13 +3,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.find(current_user.id)
 
     if @user.update_without_password(params[:user])
-      set_flash_message :notice, :updated
-      #sign_in @user, :bypass => true
-      respond_with @user, :location => after_update_path_for(@user)
+      if @user.respond_to?(:pending_reconfirmation?) && @user.pending_reconfirmation?
+        flash_key = :update_needs_confirmation
+      end
+      set_flash_message :notice, flash_key || :updated
     else
       clean_up_passwords @user
-      respond_with @user
     end
+    respond_with @user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < Model
 
   # Virtual attributes
   attr_accessor :login
-  attr_accessible :login, :avatar, :avatar_cache, :remove_avatar, :name, :date_of_birth, :bio, :password, :password_confirmation
+  attr_accessible :login, :avatar, :avatar_cache, :remove_avatar, :name, :email, :date_of_birth, :bio, :password, :password_confirmation
 
   validate  do
     add_error(:date_of_birth,:too_young) if (!self.date_of_birth.nil?) && (self.date_of_birth > 13.years.ago.to_date)
@@ -118,7 +118,6 @@ class User < Model
   end
 
   def update_without_password(params={})
-    params.delete(:email)
     super(params)
   end
 
