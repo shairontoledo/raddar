cad8199d41a9c31a4e520f6bb35cdd475acb6e99
Volmer Soares <lrvolmer@gmail.com>
2012-05-28 01:15:01 -0300

    Delayed Jobs - Begin

--- app/jobs/notify_forum_post_job.rb
+++ app/jobs/notify_forum_post_job.rb
@@ -1,13 +1,7 @@
-require 'pp'
-
 class NotifyForumPostJob < Struct.new(:post_id)
   def perform
     post = Post.find(:post_id).first
     topic = Topic.find(post.topic_id).first
-
-    pp post
-    pp topic
-
 
     post.topic.watchers.each do |watcher|
       NoticeMailer.new_forum_post_email(watcher, post).deliver if watcher != post.user
