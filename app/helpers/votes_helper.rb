ed4fd8b531ab2fc022eceb2e74f9237fc804487b
Volmer Soares <lrvolmer@gmail.com>
2012-06-15 17:57:18 -0300

    Added invitations for guest user

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -14,10 +14,13 @@ module VotesHelper
     btn_class << value.to_s
     btn_class << 'voted' if((!vote.nil?) && (vote.value == value))
 
+    title = user_signed_in? ? t("vote.#{value}") : t('user.session.invite', action: t('vote.new')) 
+
 
 
     return link_to path, 
       :class => btn_class.join(' '),
+      title: title,
       method: :post,
       remote: true do
         raw "<i class=\"#{icon}\"></i>"+
