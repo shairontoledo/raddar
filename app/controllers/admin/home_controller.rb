a89f50a2f149eee2d0bacf4c7a756752b0db445d
volmer <volmerius@gmail.com>
2012-10-09 16:18:58 -0300

    Admin index page

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,5 +3,18 @@ class Admin::HomeController < ApplicationController
 
   def index
     authorize! :index, User
+
+    @users_count = User.where(:confirmed_at.exists => true).count
+
+    @pubs_count = Pub.count
+    @stuffs_count = Stuff.count
+
+    @forums_count = Forum.count
+    @topics_count = Topic.count
+    @posts_count = Post.count
+
+    @venues_count = Venue.count
+
+    @comments_count = Comment.count
   end
 end
--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,5 +1,5 @@
 module Admin::AdminHelper
   def admin_page_title *args
-    page_title args.join(' ')+' - '+t('admin.page_title')
+    page_title [args.compact.join(' '), t('admin.page_title')].reject(&:blank?).compact.join(' - ')
   end
 end
