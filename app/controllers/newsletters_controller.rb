24959c3de4da97dd3ccf1419367c82358d1c9ae9
volmer <lrvolmer@gmail.com>
2012-10-10 00:26:33 -0300

    Newsletter template

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,7 +2,7 @@ class NewslettersController < ApplicationController
   load_and_authorize_resource
   
   def show
-    respond_with @newsletter, layout: 'mailer', render: 'newsletter_mailer/newsletter_mail'
+    respond_with @newsletter, layout: 'mailer', template: 'newsletter_mailer/newsletter_mail'
   end
 
 end
