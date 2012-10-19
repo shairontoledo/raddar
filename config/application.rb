b0716ed3b9d060849acb9607560b5b7c9639d6da
volmer <volmerius@gmail.com>
2012-10-19 17:36:01 -0300

    Changed locale files organization

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,7 @@ module Raddar
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = config.locale
     
     # Configure the default encoding used in templates for Ruby 1.9.
