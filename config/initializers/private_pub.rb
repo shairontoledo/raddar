3d5405923df5f34aa1ac7dc397291ef973822e52
volmer <lrvolmer@gmail.com>
2012-10-28 22:06:32 -0200

    PrivatePub rackup

--- config/initializers/private_pub.rb
+++ config/initializers/private_pub.rb
@@ -2,5 +2,5 @@
 PrivatePub.setup do |private_pub|
   private_pub.config['server'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['server'] : 'http://localhost:9292/faye'
   private_pub.config['secret_token'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['secret_token'] : 'secret_token'
-  private_pub.config['signature_expiration'] = 3600 # one hour
+  private_pub.config['signature_expiration'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['signature_expiration'] : 3600
 end
