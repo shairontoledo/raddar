8145f8b77eeaa4152835d9dc28b798162cd80b68
volmer <lrvolmer@gmail.com>
2012-11-01 23:13:43 -0200

    Configuration for PrivatePub

--- config/initializers/private_pub.rb
+++ config/initializers/private_pub.rb
@@ -1,6 +1,2 @@
 # Use this setup block to configure all options available in PrivatePub.
-PrivatePub.setup do |private_pub|
-  private_pub.config['server'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['server'] : 'http://localhost:9292/faye'
-  private_pub.config['secret_token'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['secret_token'] : 'secret_token'
-  private_pub.config['signature_expiration'] = Raddar::config.respond_to?(:private_pub) ? Raddar::config.private_pub['signature_expiration'] : 3600
-end
+PrivatePub.load_config(Rails.root.join("raddar.yml"), "private_pub")
