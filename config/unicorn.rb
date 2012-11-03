1dc3439928f4c544171393d0beaf4750219dca53
volmer <lrvolmer@gmail.com>
2012-11-03 17:30:59 -0200

    Config for Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,11 @@
-listen 5000
-worker_processes 4
+require 'yaml'
+
+raddar_unicorn_default = {'listen' => 5000, 'worker_processes' => 4}
+
+raddar_unicorn = YAML.load_file(File.expand_path("../../raddar.yml", __FILE__))['unicorn'] || raddar_unicorn_default
+
+raddar_unicorn = raddar_unicorn_default.merge(raddar_unicorn)
+
+
+listen raddar_unicorn['listen']
+worker_processes raddar_unicorn['worker_processes']
